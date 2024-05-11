18aee55c4c8bbf93d3cfb5b1000100cc7719ceb3
lucasddsdev <lkssilva2254@gmail.com>
2024-05-11 17:56:58 -0300

    Executando AboutMe pelo prompt

---
+++ MinhaClasse/src/MinhaClasse.java
@@ -0,0 +1,13 @@
+public class MinhaClasse {
+    public static void main(String[] args) {
+
+        String nome = args [0];
+
+        String sobrenome = args [1];
+
+        int idade = Integer.valueOf(args [2]);
+
+
+        System.out.println("Oi, meu nome eh "+ nome + " " + sobrenome + " e tenho " + idade + " anos");
+    }
+}
